2d46738b90f430b801eb61c07413b640d96521cd
amirtbi <torabi46@gmail.com>
2022-04-10 22:43:26 +0430

    define components

---
+++ src/components/HomeItems/Gallery.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="d-flex flex-column mt-5 pa-16">
+    <!-- Section title -->
+    <div class="d-flex flex-column justify-center align-center mb-10">
+      <h5 class="mb-2 text-center text-h4">Gallery</h5>
+      <p class="text-body-1 text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit
+      </p>
+    </div>
+    <!-- Image galleries -->
+    <div class="d-flex flex-row align-center mx-5 justify-center">
+      <!-- IMAGE CONTAINE -->
+      <div style="width: 100%" v-for="(image, index) in images" :key="index">
+        <v-hover v-slot="{ isHovering, props }">
+          <v-card
+            :elevation="isHovering ? 12 : 2"
+            class="cursor-pointer d-flex mx-2"
+            :class="{ 'on-hover': isHovering }"
+            v-bind="props"
+          >
+            <v-img
+              :src="'../../src/assets/images/' + image.src + '.jpg'"
+              height="150px"
+              aspect-ratio="2"
+              cover
+              class="cursor-pointer"
+            ></v-img>
+          </v-card>
+        </v-hover>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      images: [
+        { src: "gallery-1" },
+        { src: "gallery-2" },
+        { src: "gallery-3" },
+        { src: "gallery-4" },
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.v-card {
+  cursor: pointer;
+}
+img:hover {
+  box-shadow: 0 0 0 2px darkgray;
+}
+</style>
